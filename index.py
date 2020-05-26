1111904d81d4fdfbcf87240193442c3402cc49b3
bwlodarski277 <b.wlodarski277@gmail.com>
2020-05-26 13:14:28 +0000

    Cleaned index file
    Redundant imports and whitepace has been removed.

--- index.py
+++ index.py
@@ -1,6 +1,4 @@
 import discord
-import praw
-import prawcore.exceptions as pce
 import json
 import os
 from datetime import datetime
@@ -12,11 +10,6 @@ with open('config.json') as config:
     prefix, token, reddit_oauth = json.load(config).values()
 
 reddit_oauth = None
-
-reddit = praw.Reddit('SubredditBot')
-
-sorts = ['new', 'rising', 'hot', 'top', 'random']
-times = ['hour', 'day', 'week', 'month', 'year', 'all']
 
 bot = commands.Bot(command_prefix=prefix)
 
@@ -32,7 +25,7 @@ async def on_ready():
     for filename in os.listdir('./cogs'):
         if filename.endswith('.py'):
             bot.load_extension(f'cogs.{filename[:-3]}')
-    
+    print(f'[{datetime.now()}] Logged in as {bot.user.name}')
 
 
 bot.run(token)
